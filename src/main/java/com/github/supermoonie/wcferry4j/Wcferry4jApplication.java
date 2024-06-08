6011b795e270b95babbe5597f59a8d2f3c591122
super_wang <wangchao@jiean.net>
2024-06-08 09:22:50 +0800

    init

---
+++ src/main/java/com/github/supermoonie/wcferry4j/Wcferry4jApplication.java
@@ -0,0 +1,13 @@
+package com.github.supermoonie.wcferry4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wcferry4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Wcferry4jApplication.class, args);
+    }
+
+}
